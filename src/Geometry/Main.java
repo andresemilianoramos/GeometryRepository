c39dad89a5cbb5021d97a083a0e38e31abfc138c
andresemiliano1 <andresemiliano1@gmail.com>
2022-11-06 12:52:07 +0100

    A.Ramos hasta el enunciado 23

---
+++ src/Geometry/Main.java
@@ -0,0 +1,24 @@
+package Geometry;
+
+public class Main {
+    public static void main(String[] args) {
+        Segmento p = new Segmento();
+        p.setStartPoint(4);
+        p.setEndPoint(5);
+        p.setOffset(4,4);
+
+        Segmento p2 = new Segmento();
+        p2.setStartPoint(6);
+        p2.setEndPoint(8);
+        p2.setOffset(4,4);
+
+       System.out.println("La 1 coordenada es: "+p.getStartPoint()+","+p.getEndPoint());
+       System.out.println("La 2 coordenada es: "+p2.getStartPoint()+","+p2.getEndPoint());
+
+        System.out.println("");
+        System.out.println("(" +p.startPoint+","+p.endPoint+") - ("+p2.startPoint+','+ p2.endPoint+')');
+        System.out.println("\nLa distancia es: "+p.calcularDistancia(p2));
+
+
+    }
+}
---
+++ src/Geometry/Segmento.java
@@ -0,0 +1,38 @@
+package Geometry;
+
+/*
+ Teorema de Pitagoras para dos puntos:
+
+ Punto1X = (x1, y1)
+ Punto2Y = (x2, y2)
+
+ distancia XY = raiz((x2-x1)^2 + (y2-y1)^2)
+
+
+ */
+
+public class Segmento { //Create the class Segment which has 2 points
+  public int startPoint; //// (startPoint
+  public int endPoint; //and endPoint)
+
+  public void setStartPoint(int startPoint){this.startPoint = startPoint;}
+  public void  setEndPoint(int endPoint){this.endPoint = endPoint;}
+  public int getEndPoint() {return endPoint;}
+  public int getStartPoint() {return startPoint;}
+  public void setOffset(int offX , int offY){ startPoint += + offX; endPoint += + offY;}
+
+
+  public double calcularDistancia(Segmento p){
+    int startPoint1=startPoint;
+    int endPoint1=endPoint;
+    int startPoint2=p.getStartPoint();
+    int endPoint2=p.getEndPoint();
+    double distancia= Math.sqrt(Math.pow(startPoint2-startPoint1,2)+Math.pow(endPoint2-endPoint1,2));
+
+    return distancia;}
+
+  @Override
+  public String toString() {
+    return '('+ startPoint + "," + endPoint + ')';
+  }
+}
